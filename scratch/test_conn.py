12bafadef61d0adeeb28fa8a50e928862e39b71e
krish0125 <kishu4kamlesh@gmail.com>
2026-07-18 17:11:58 +0530

    Clean up dev/debug artifacts before deployment

--- scratch/test_conn.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-try:
-    res = requests.get("http://127.0.0.1:5500/", timeout=5)
-    print("Status Code:", res.status_code)
-    print("Content length:", len(res.content))
-except Exception as e:
-    print("Error:", e)
